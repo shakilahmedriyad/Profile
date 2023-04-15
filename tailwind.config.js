64c8db198bede072b69aa6ca3385b7c6c45680d7
Riyad139 <riyad.ahmed1886@gmail.com>
2023-04-16 00:47:29 +0600

    all the lock and package is added

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{tsx,jsx,ts,js}",
+    "./components/**/*.{tsx,ts,js,jsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
