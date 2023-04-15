8b7b30c7ab4ffbfb56e6641084c6691423c5f580
Riyad139 <riyad.ahmed1886@gmail.com>
2023-04-16 00:48:06 +0600

    dockcs ar added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,16 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { Poppins } from "next/font/google";
+
+const poppins = Poppins({
+  subsets: ["devanagari"],
+  weight: ["400", "500", "600", "700"],
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main className={poppins.className}>
+      <Component {...pageProps} />;
+    </main>
+  );
 }
