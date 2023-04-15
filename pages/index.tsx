e12306befc11fcd7a28a999c0be0b81a1bc25982
Riyad139 <riyad.ahmed1886@gmail.com>
2023-04-16 02:42:56 +0600

    svg are added

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,11 @@
 import Header from "@/components/Header";
+import TechNology from "@/components/TechNology";
 
 export default function Home() {
   return (
     <>
       <Header />
+      <TechNology />
     </>
   );
 }
