25843425f4fab338cd11b0289ac0bc0ca9f60e14
Riyad139 <riyad.ahmed1886@gmail.com>
2023-04-17 23:27:39 +0600

    contact page responsive done

--- components/Contacts/index.tsx
+++ components/Contacts/index.tsx
@@ -42,57 +42,56 @@ export default function Contacts() {
     }
   };
   return (
-    <div className=" py-7 mt-28 mb-10 text-gray-100 flex flex-col justify-center items-center  ">
+    <div className=" w-full py-7 mt-28 mb-10 text-gray-100 flex flex-col justify-center items-center  ">
       <Toaster position="top-right" />
-      <div className="px-24 shadow-xl py-16 rounded-md bg-[#171717] ">
-        <div className=" w-[80%] sm:w-[60%] md:w-[35rem] text-sm ">
-          <div className="space-y-2">
-            <p>Interested in a collabaration?</p>
-            <p className="text-4xl bg-gradient-to-tr  text-transparent bg-clip-text from-gray-50 via-gray-300 to-gray-500 font-medium mt-7">
-              Contact Me
-            </p>
-          </div>
-          <div className="w-full space-y-2 mt-10">
-            <p>What should I call you ?</p>
-            <TextInput
-              className="!bg-[#1F1F1F] !text-gray-200 !border-none"
-              height={55}
-              //@ts-ignore
-              ref={nameRef}
-              placeholder="Tonny Stark"
-              width={"100%"}
-            />
-          </div>
-          <div className="mt-7 space-y-2">
-            <p>Your email address ?</p>
-            <TextInput
-              className="!bg-[#1F1F1F] !text-gray-200 !border-none"
-              height={55}
-              //@ts-ignore
-              ref={emailRef}
-              type="email"
-              placeholder="Iron@Man.com"
-              width={"100%"}
-            />
-          </div>
-          <div className="mt-7 space-y-2">
-            <p>Your message</p>
-            <Textarea
-              rows={7}
-              //@ts-ignore
-              ref={messageRef}
-              resize={"none"}
-              placeholder="Want to know about yourself"
-              className="!bg-[#1F1F1F] !text-base !text-gray-200 !border-none"
-            />
-          </div>
-          <Button
-            onClick={onSubmit}
-            className="mt-7 bg-gradient-to-br !py-5 !px-12 !border-gray-500 !text-base !text-gray-200 from-[#464444] via-[#1F1F1F] to-[#1F1F1F] w-20 !rounded-full"
-          >
-            Send
-          </Button>
+
+      <div className=" w-[80%] sm:w-[60%] md:w-[46rem] bg-[#171717] px-9 md:px-24 shadow-xl py-16 rounded-xl text-sm ">
+        <div className="space-y-2">
+          <p>Interested in a collabaration?</p>
+          <p className="text-4xl bg-gradient-to-tr  text-transparent bg-clip-text from-gray-50 via-gray-300 to-gray-500 font-medium mt-7">
+            Contact Me
+          </p>
         </div>
+        <div className="w-full space-y-2 mt-10">
+          <p>What should I call you ?</p>
+          <TextInput
+            className="!bg-[#1F1F1F] !text-gray-200 !border-none"
+            height={55}
+            //@ts-ignore
+            ref={nameRef}
+            placeholder="Tonny Stark"
+            width={"100%"}
+          />
+        </div>
+        <div className="mt-7 space-y-2">
+          <p>Your email address ?</p>
+          <TextInput
+            className="!bg-[#1F1F1F] !text-gray-200 !border-none"
+            height={55}
+            //@ts-ignore
+            ref={emailRef}
+            type="email"
+            placeholder="Iron@Man.com"
+            width={"100%"}
+          />
+        </div>
+        <div className="mt-7 space-y-2">
+          <p>Your message</p>
+          <Textarea
+            rows={7}
+            //@ts-ignore
+            ref={messageRef}
+            resize={"none"}
+            placeholder="Want to know about yourself"
+            className="!bg-[#1F1F1F] !text-base !text-gray-200 !border-none"
+          />
+        </div>
+        <Button
+          onClick={onSubmit}
+          className="mt-7 bg-gradient-to-br !py-5 !px-12 !border-gray-500 !text-base !text-gray-200 from-[#464444] via-[#1F1F1F] to-[#1F1F1F] w-20 !rounded-full"
+        >
+          Send
+        </Button>
       </div>
     </div>
   );
